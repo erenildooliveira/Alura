54ce261d2dead051dad77d2a74194ad455d42b7f
Erenildo Oliveira Pinto Neto <erenildoneto07@gmail.com>
2024-07-10 17:24:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+//let titulo = document.querySelector('h1')
+//titulo.innerHTML = 'Jogo do Número Secreto';
+
+//let paragrafo = document.querySelector('p')
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Miseravi, acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute ('disabled');
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor.');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior.');
+        }
+        tentativas++;
+        limparCampo()
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
